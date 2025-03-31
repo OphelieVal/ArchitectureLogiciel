06bbbc43d2e9b0b336a821585a78f0b4e99daa8f
OphelieVal <ophelie.valin@etu.uni-orleans.fr>
2025-03-31 09:03:15 +0200

    Modification suppression

--- quiz/projettodos/src/components/SupprQuestionnaire.vue
+++ quiz/projettodos/src/components/SupprQuestionnaire.vue
@@ -14,9 +14,6 @@
 <template>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <div class="container">
-          <label>
-            {{ questionnaire.name }}
-          </label>
           <input type="button" class="btn btn-danger" value="Supprimer" @click="suppr" >
     </div>
 </template>
