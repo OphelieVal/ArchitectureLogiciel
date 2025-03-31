cf9f922606ce1c262d0362458af347c8b98d0da7
OphelieVal <ophelie.valin@etu.uni-orleans.fr>
2025-03-31 08:58:02 +0200

    Ajout de la suppression d'un questionnaire + composent

--- quiz/projettodos/src/App.vue
+++ quiz/projettodos/src/App.vue
@@ -1,12 +1,26 @@
 <script>
+import SupprQuestionnaire from './components/SupprQuestionnaire.vue';
 
 let data = {
-  questionnaires: [{id: 0, name: "hello"},{id: 1, name: "questionnaire"}]
+  questionnaires: []
 }
 
 export default {
   data(){
     return data;
+  },
+  methods : {
+    get_by_id: function(id) {
+        for (let i = 0; i < this.questionnaires.length; i++) {
+          if (this.questionnaires[i].id == id) {
+            return this.questionnaires[i];
+          }
+        }
+        return null;
+      },
+    remove_questionnaires($event) {
+        this.questionnaires.splice(this.questionnaires.indexOf(this.get_by_id($event.id)), 1)
+      },
   },
   mounted(){
     fetch('http://localhost:5000/quiz/api/v1.0/questionnaire')
@@ -14,13 +28,22 @@ export default {
     .then(json => {
       this.questionnaires = json
     })
-  }
+  },
+  components : {SupprQuestionnaire}
 }
 </script>
 
 <template>
-  <h1>Hello</h1>
-  <li v-for="question in questionnaires"> {{ question.name }} </li>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/
+dist/css/bootstrap.min.css" integrity="sha384-
+rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin
+="anonymous">
+  <div class="container">
+    <h1>Hello</h1>
+    <ol>
+        <SupprQuestionnaire v-for="item of questionnaires" :questionnaire="item" @remove="remove_questionnaires($event)"></SupprQuestionnaire>
+    </ol>
+  </div>
   
 </template>
 
