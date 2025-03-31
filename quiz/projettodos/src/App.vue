0034d9295f71ef008aeb20aac54c63ae64510fef
Naima Akhtar <naimasanaakhtar@gmail.com>
2025-03-31 08:46:59 +0200

    ajout questionnaire

--- quiz/projettodos/src/App.vue
+++ quiz/projettodos/src/App.vue
@@ -1,5 +1,7 @@
 <script>
 
+import AjouterQuestionnaire from './components/AjouterQuestionnaire.vue';
+
 let data = {
   questionnaires: [{id: 0, name: "hello"},{id: 1, name: "questionnaire"}]
 }
@@ -7,6 +9,18 @@ let data = {
 export default {
   data(){
     return data;
+  },
+  methods: {
+    addQuestionnaire: function () {
+      let text = this.newItem.trim();
+      if (text) {
+        this.todos.push({
+          text: text,
+          checked: false
+        });
+        this.newItem = '';
+      }
+    }
   },
   mounted(){
     fetch('http://localhost:5000/quiz/api/v1.0/questionnaire')
@@ -20,8 +34,13 @@ export default {
 
 <template>
   <h1>Hello</h1>
-  <li v-for="question in questionnaires"> {{ question.name }} </li>
-  
+  <li v-for="question in questionnaires"> 
+    {{ question.name }} 
+  <AjouterQuestionnaire>
+      v-for="q of questionnaires" :questionnaire="q"
+      @add="addQuestionnaire">
+  </AjouterQuestionnaire>
+  </li>
 </template>
 
 
---
+++ quiz/projettodos/src/components/AjouterQuestionnaire.vue
@@ -0,0 +1,28 @@
+<script>
+export default{
+    props: {
+        questionnaire : Object
+    },
+    methods : {
+        ajouter: function() {
+          this.$emit('add',{id:this.questionnaire.id});
+        }
+    },
+    emits : ['add']
+}
+</script>
+<template>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <div class="input-group">
+        <input v-model="newItem"
+            @keyup.enter="add" 
+            placeholder="Ajouter un questionnaire à la liste"
+            type="text"
+            class="form-control">
+        <span class="input-group-btn">
+            <button @click="add"
+            class="btn btn-default"
+            type="button">Ajouter</button>
+        </span>
+    </div>
+</template>
